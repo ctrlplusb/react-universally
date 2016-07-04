eb51d61d0bcf7f1d7f5dd1c090b5eef15918ccd8
Sean Matheson <sean@ctrlplusb.com>
2016-07-04 11:29:33 +0100

    adds hot reloading support of the webpack configurations.

--- devServer.js
+++ devServer.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console,global-require */
+/* eslint-disable no-console,global-require,no-underscore-dangle */
 
 const path = require('path');
 const notifier = require('node-notifier');
@@ -7,145 +7,233 @@ const webpack = require('webpack');
 const express = require('express');
 const createWebpackMiddleware = require('webpack-dev-middleware');
 const createWebpackHotMiddleware = require('webpack-hot-middleware');
-const clientBundleConfig = require('./webpack.client.config')({ mode: 'development' });
-const serverBundleConfig = require('./webpack.server.config')({ mode: 'development' });
 
 function createNotification(subject, msg) {
-  const title = `${subject.toUpperCase()} DEVSERVER`;
-
-  console.log(`==> ${title} -> ${msg}`);
+  const title = `🔥  ${subject.toUpperCase()}`;
 
   notifier.notify({
     title,
     message: msg,
   });
+
+  console.log(`==> ${title} -> ${msg}`);
 }
 
-/**
- * -----------------------------------------------------------------------------
- * Server Bundle Server
- */
-let serverListener = null;
-let lastConnectionKey = 0;
-const connectionMap = {};
-const serverBundleCompiler = webpack(serverBundleConfig);
-const serverBundlePath = path.resolve(
-  serverBundleConfig.output.path, `${Object.keys(serverBundleConfig.entry)[0]}.js`
-);
-
-serverBundleCompiler.plugin('done', (stats) => {
-  if (stats.hasErrors()) {
-    createNotification('server', '😵  Build failed, check console for error');
-    console.log(stats.toString());
-    return;
-  }
-
-  createNotification('server', '✅  Bundle built');
-
-  // Make sure our newly built server and client bundles aren't in the module cache.
-  Object.keys(require.cache).forEach(modulePath => {
-    if (~modulePath.indexOf(serverBundleConfig.output.path)) {
-      delete require.cache[modulePath];
-    }
-  });
-
-  // We wrap the require and execution of our server bundle just in case it
-  // contains invalid code that throws an exception.  Rather than taking down
-  // the devServer this gives us the opportunity to fix the error and have
-  // the server bundle start again automatically.
-  try {
-    // The server bundle  will automatically start the web server just by
-    // requiring it.
-    serverListener = require(serverBundlePath).default;
+class ListenerManager {
+  constructor(listener) {
+    this.lastConnectionKey = 0;
+    this.connectionMap = {};
+    this.listener = listener;
 
     // Track all connections to our server so that we can close them when needed.
-    serverListener.on('connection', connection => {
+    this.listener.on('connection', connection => {
       // Generate a new key to represent the connection
-      const connectionKey = ++lastConnectionKey;
+      const connectionKey = ++this.lastConnectionKey;
       // Add the connection to our map.
-      connectionMap[connectionKey] = connection;
+      this.connectionMap[connectionKey] = connection;
       // Remove the connection from our map when it closes.
       connection.on('close', () => {
-        delete connectionMap[connectionKey];
+        delete this.connectionMap[connectionKey];
       });
     });
-
-    createNotification('server', '✅  Running');
-  } catch (err) {
-    createNotification('server', '😵  Bundle invalid, check console for error');
-    console.log(err);
   }
-});
 
-function serverListenerDispose(callback) {
-  // First we destroy any connections.
-  Object.keys(connectionMap).forEach((connectionKey) => {
-    connectionMap[connectionKey].destroy();
-  });
+  dispose() {
+    return new Promise(resolve => {
+      // First we destroy any connections.
+      Object.keys(this.connectionMap).forEach((connectionKey) => {
+        this.connectionMap[connectionKey].destroy();
+      });
 
-  // Then we close the server.
-  if (serverListener) {
-    serverListener.close(() => {
-      if (callback) callback();
+      // Then we close the listener.
+      if (this.listener) {
+        this.listener.close(() => {
+          resolve();
+        });
+      } else {
+        resolve();
+      }
     });
-  } else {
-    callback();
   }
 }
 
-function compileServerBundle() {
-  // Shut down any existing running server if necessary before starting the
-  // compile, else just compile.
-  if (serverListener) {
-    serverListenerDispose(() => serverBundleCompiler.run(() => undefined));
-  } else {
-    serverBundleCompiler.run(() => undefined);
+class HotServer {
+  constructor(compiler) {
+    this.compiler = compiler;
+    this.listenerManager = null;
+
+    const compiledOutputPath = path.resolve(
+      compiler.options.output.path, `${Object.keys(compiler.options.entry)[0]}.js`
+    );
+
+    try {
+      // The server bundle  will automatically start the web server just by
+      // requiring it. It returns the http listener too.
+      this.listenerManager = new ListenerManager(require(compiledOutputPath).default);
+
+      createNotification('server', '✅  Running');
+    } catch (err) {
+      createNotification('server', '😵  Bundle invalid, check console for error');
+      console.log(err);
+    }
+  }
+
+  dispose() {
+    return Promise.all([
+      this.listenerManager ? this.listenerManager.dispose() : undefined,
+    ]);
   }
 }
 
-// Now we will configure `chokidar` to watch our server specific source folder.
-// Any changes will cause a rebuild of the server bundle.
-const watcher = chokidar.watch([path.resolve(__dirname, './src/server')]);
+class HotClient {
+  constructor(compiler) {
+    const app = express();
+    this.webpackDevMiddleware = createWebpackMiddleware(compiler, {
+      quiet: true,
+      noInfo: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      // The path at which the client bundles are served from.  Note: in this
+      // case as we are running a seperate dev server the public path should
+      // be absolute, i.e. including the "http://..."
+      publicPath: compiler.options.output.publicPath,
+    });
+    app.use(this.webpackDevMiddleware);
+    app.use(createWebpackHotMiddleware(compiler));
+
+    const listener = app.listen(process.env.CLIENT_DEVSERVER_PORT);
+    this.listenerManager = new ListenerManager(listener);
+
+    createNotification('client', '✅  Running');
+  }
+
+  dispose() {
+    this.webpackDevMiddleware.close();
+
+    return Promise.all([
+      this.listenerManager ? this.listenerManager.dispose() : undefined,
+    ]);
+  }
+}
+
+class HotServers {
+  constructor() {
+    // Bind our functions to avoid any scope/closure issues.
+    this.start = this.start.bind(this);
+    this.restart = this.restart.bind(this);
+    this._configureHotClient = this._configureHotClient.bind(this);
+    this._configureHotServer = this._configureHotServer.bind(this);
+
+    this.clientBundle = null;
+    this.clientCompiler = null;
+    this.serverBundle = null;
+    this.serverCompiler = null;
+  }
+
+  start() {
+    try {
+      const clientConfig = require('./webpack.client.config')({ mode: 'development' });
+      this.clientCompiler = webpack(clientConfig);
+      const serverConfig = require('./webpack.server.config')({ mode: 'development' });
+      this.serverCompiler = webpack(serverConfig);
+    } catch (err) {
+      createNotification('webpack', '😵  Webpack config invalid, check console for error');
+      console.log(err);
+      return;
+    }
+
+    this._configureHotClient();
+    this._configureHotServer();
+  }
+
+  restart() {
+    const clearWebpackConfigsCache = () => {
+      Object.keys(require.cache).forEach(modulePath => {
+        if (~modulePath.indexOf('webpack')) {
+          delete require.cache[modulePath];
+        }
+      });
+    };
+
+    Promise.all([
+      this.serverBundle ? this.serverBundle.dispose() : undefined,
+      this.clientBundle ? this.clientBundle.dispose() : undefined,
+    ]).then(clearWebpackConfigsCache).then(this.start, err => console.log(err));
+  }
+
+  _configureHotClient() {
+    this.clientCompiler.plugin('done', (stats) => {
+      if (stats.hasErrors()) {
+        createNotification('client', '😵  Build failed, check console for error');
+        console.log(stats.toString());
+      } else {
+        createNotification('client', '✅  Built');
+      }
+    });
+
+    this.clientBundle = new HotClient(this.clientCompiler);
+  }
+
+  _configureHotServer() {
+    const compileHotServer = () => {
+      const runCompiler = () => this.serverCompiler.run(() => undefined);
+      // Shut down any existing running server if necessary before starting the
+      // compile, else just compile.
+      if (this.serverBundle) {
+        this.serverBundle.dispose().then(runCompiler);
+      } else {
+        runCompiler();
+      }
+    };
+
+    this.clientCompiler.plugin('done', (stats) => {
+      if (!stats.hasErrors()) {
+        compileHotServer();
+      }
+    });
+
+    this.serverCompiler.plugin('done', (stats) => {
+      if (stats.hasErrors()) {
+        createNotification('server', '😵  Build failed, check console for error');
+        console.log(stats.toString());
+      } else {
+        createNotification('server', '✅  Built');
+      }
+
+      // Make sure our newly built server bundles aren't in the module cache.
+      Object.keys(require.cache).forEach(modulePath => {
+        if (~modulePath.indexOf(this.serverCompiler.options.output.path)) {
+          delete require.cache[modulePath];
+        }
+      });
+
+      this.serverBundle = new HotServer(this.serverCompiler);
+    });
+
+    // Now we will configure `chokidar` to watch our server specific source folder.
+    // Any changes will cause a rebuild of the server bundle.
+    this.watcher = chokidar.watch([path.resolve(__dirname, './src/server')]);
+    this.watcher.on('ready', () => {
+      this.watcher
+        .on('add', compileHotServer)
+        .on('addDir', compileHotServer)
+        .on('change', compileHotServer)
+        .on('unlink', compileHotServer)
+        .on('unlinkDir', compileHotServer);
+    });
+  }
+}
+
+const hotServers = new HotServers();
+
+// Any changes to our webpack config builder will cause us to restart our
+// hot servers.
+const watcher = chokidar.watch(
+  path.resolve(__dirname, './webpackConfigFactory.js')
+);
 watcher.on('ready', () => {
-  watcher
-    .on('add', compileServerBundle)
-    .on('addDir', compileServerBundle)
-    .on('change', compileServerBundle)
-    .on('unlink', compileServerBundle)
-    .on('unlinkDir', compileServerBundle);
+  watcher.on('change', hotServers.restart);
 });
 
-/**
- * -----------------------------------------------------------------------------
- * Client Bundle Server
- */
-const clientBundleCompiler = webpack(clientBundleConfig);
-clientBundleCompiler.plugin('done', (stats) => {
-  if (stats.hasErrors()) {
-    createNotification('client', '😵  Build failed, check console for error');
-    console.log(stats.toString());
-  } else {
-    createNotification('client', '✅  Built');
-
-    // We will also build the server bundle any time the client bundle has
-    // been built.  We do this as the server bundle depends on the client bundle,
-    // so if the client switched from an invalid to a valid state we want our
-    // server bundle to restart too.
-    compileServerBundle();
-  }
-});
-const clientBundleServer = express();
-const webpackDevMiddleware = createWebpackMiddleware(clientBundleCompiler, {
-  quiet: true,
-  noInfo: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-  // The path at which the client bundles are served from.  Note: in this
-  // case as we are running a seperate dev server the public path should
-  // be absolute, i.e. including the "http://..."
-  publicPath: clientBundleConfig.output.publicPath,
-});
-clientBundleServer.use(webpackDevMiddleware);
-clientBundleServer.use(createWebpackHotMiddleware(clientBundleCompiler));
-clientBundleServer.listen(process.env.CLIENT_DEVSERVER_PORT);
+hotServers.start();
