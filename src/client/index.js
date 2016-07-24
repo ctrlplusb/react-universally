17a35312e6a25e92c7f3135686325941371904c6
Sean Matheson <sean@ctrlplusb.com>
2016-07-24 09:21:52 +0100

    Changes the client side router configuration to handle redirect and "no renderProps" cases.  Fixes #29

--- src/client/index.js
+++ src/client/index.js
@@ -9,27 +9,36 @@ import routes from '../shared/routes';
 // Get the DOM Element that will host our React application.
 const container = document.querySelector('#app');
 
+function routerError(error) {
+  // TODO: Error handling.
+  console.error('==> 😭  React Router match failed.'); // eslint-disable-line no-console
+  if (error) { console.error(error); } // eslint-disable-line no-console
+}
+
 function renderApp() {
   // As we are using dynamic react-router routes we have to use the following
   // asynchronous routing mechanism supported by the `match` function.
   // @see https://github.com/reactjs/react-router/blob/master/docs/guides/ServerRendering.md
   match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
     if (error) {
-      // TODO: Error handling.
-      console.log('==> 😭  React Router match failed.'); // eslint-disable-line no-console
+      routerError(error);
+    } else if (redirectLocation) {
+      return;
+    } else if (renderProps) {
+      render(
+        <AppContainer>
+          {/*
+          We need to explicly render the Router component here instead of have
+          this embedded within a shared App type of component as we use different
+          router base components for client vs server rendering.
+          */}
+          <Router {...renderProps} />
+        </AppContainer>,
+        container
+      );
+    } else {
+      routerError();
     }
-
-    render(
-      <AppContainer>
-        {/*
-        We need to explicly render the Router component here instead of have
-        this embedded within a shared App type of component as we use different
-        router base components for client vs server rendering.
-        */}
-        <Router {...renderProps} />
-      </AppContainer>,
-      container
-    );
   });
 }
 
