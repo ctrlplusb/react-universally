7fe8b0ad17b4e1af0fa3b8c7a4156aa6bfae1f5a
Sean Matheson <sean@ctrlplusb.com>
2016-09-26 10:08:45 +0100

    Fixes eslint, updating to latest deps.

--- src/shared/components/App/views/Home/Home.js
+++ src/shared/components/App/views/Home/Home.js
@@ -11,7 +11,7 @@ function Home() {
       <p>
         This starter kit contains all the build tooling and configuration you
         need to kick off your next universal react project, whilst containing a
-        minimal "project" set up allowing you to make your own architecture
+        minimal project set up allowing you to make your own architecture
         decisions (redux/mobx etc).
       </p>
     </article>
