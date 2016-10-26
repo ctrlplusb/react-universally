e2d3daff4e35eb23466b9db12967e0e705a41a21
Sean Matheson <sean@ctrlplusb.com>
2016-10-26 15:14:23 +0100

    Adds mobile friendly meta tag.

--- src/universalMiddleware/render.js
+++ src/universalMiddleware/render.js
@@ -61,6 +61,7 @@ function render(reactAppElement : ?ReactElement, initialState : ?Object) {
     <html ${helmet ? helmet.htmlAttributes.toString() : ''}>
       <head>
         <meta charSet='utf-8' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta httpEquiv='Content-Language' content='en' />
         <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
