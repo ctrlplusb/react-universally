4771e5fccf5e210fd4d14dcecad9bab44d0b08c4
Sean Matheson <sean@ctrlplusb.com>
2016-10-27 16:39:02 +0100

    Cleans up the render function, moving as many elements as possible into our App component.

--- src/shared/universal/components/App/App.js
+++ src/shared/universal/components/App/App.js
@@ -22,7 +22,26 @@ function App() {
         titleTemplate={`${WEBSITE_TITLE} - %s`}
         defaultTitle={WEBSITE_TITLE}
         meta={[
+          { name: 'charset', content: 'utf-8' },
+          // This is important to signify your application is mobile responsive!
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
           { name: 'description', content: WEBSITE_DESCRIPTION },
+          // Providing a theme color is good if you are doing a progressive
+          // web application.
+          { name: 'theme-color', content: '#2b2b2b' },
+        ]}
+        link={[
+          // When building a progressive web application you need to supply
+          // a manifest.json as well as a host of icon types. This can be
+          // tricky. Luckily there is a service to help you with this.
+          // http://realfavicongenerator.net/
+          // Go there and generate your icon pack and then extract them into
+          // the public folder.
+          { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+          { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+          { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+          { rel: 'manifest', href: '/manifest.json' },
+          { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#00a9d9' },
         ]}
         script={[]}
       />
