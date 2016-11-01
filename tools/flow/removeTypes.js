e95b87afec22c20ac9bea5fdbbb15349d88316da
Alin Porumb <alin.porumb@gmail.com>
2016-11-02 00:14:53 +0200

    Fixes missing recursive-readdir dependency needed for npm run flow:re… (#153)
    * Fixes missing recursive-readdir dependency needed for npm run flow:remove script
    
    * Replaces 'recursive-readdir' dependency with 'glob'

--- tools/flow/removeTypes.js
+++ tools/flow/removeTypes.js
@@ -1,18 +1,21 @@
-const recursive = require('recursive-readdir');
+const globSync = require('glob').sync;
 const path = require('path');
+const appRootPath = require('app-root-path').toString();
 const flowRemoveTypes = require('flow-remove-types');
 const fs = require('fs');
 const rimraf = require('rimraf');
 
 const isJsFile = file => path.extname(file) === '.js';
 
-const stripTypes = () =>
-  recursive('src/', (err, files) => files.filter(isJsFile).forEach(file => {
+const stripTypes = () => {
+  var files = globSync(`${path.resolve(appRootPath, 'src')}/**/*.js`);
+  files.filter(isJsFile).forEach(file => {
     console.log(`Removing types from "${file}`);
     var input = fs.readFileSync(file, 'utf8');
     var output = flowRemoveTypes(input);
     fs.writeFileSync(file, output);
-  }));
+  });
+};
 
 if (fs.existsSync('src/shared/universal/types')) {
   rimraf('src/shared/universal/types', (err) => {
