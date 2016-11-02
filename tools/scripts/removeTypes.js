50b2b68e0ffb961ab35c7ad2c2b6bb43fdb2908e
Sean Matheson <sean@ctrlplusb.com>
2016-11-02 11:41:36 +0000

    Updates the flow remove types script, making it far more robust.

--- tools/scripts/removeTypes.js
+++ tools/scripts/removeTypes.js
@@ -6,40 +6,69 @@ const fs = require('fs');
 const rimraf = require('rimraf');
 const { exec } = require('../utils');
 
+function safeDelete(target, cb) {
+  if (fs.existsSync(target)) {
+    rimraf(target, (err) => {
+      if (err) {
+        console.log('Failed to delete:', target);
+      }
+      if (cb) {
+        cb();
+      }
+    });
+  }
+}
+
 const srcPath = path.resolve(appRootPath, 'src');
+const flowConfigPath = path.resolve(appRootPath, '.flowconfig');
+const packageJsonPath = path.resolve(appRootPath, 'package.json');
+const flowToolsPath = path.resolve(appRootPath, 'tools/flow');
+const flowTypesPath = path.resolve(appRootPath, 'src/shared/universal/types');
+const flowScriptPath = path.resolve(appRootPath, 'tools/scripts/flow.js');
 
 const isJsFile = file => path.extname(file) === '.js';
 
-const stripTypes = () => {
-  globSync(`${path.resolve(appRootPath, 'src')}/**/*.js`)
-    .filter(isJsFile)
-    .forEach((file) => {
-      console.log(`Removing types from "${file}`);
-      const input = fs.readFileSync(file, 'utf8');
-      const output =
-        // Remove flow annotations
-        flowRemoveTypes(input)
-        // Remove the empty flow tags.
-        .replace(/\/\*\s+\*\/\n/g, '')
-        // Remove any blank lines at top of file.
-        .replace(/^\n+/, '')
-        // Remove any multiple blank lines in files.
-        .replace(/\n\n\n/g, '\n\n');
-      fs.writeFileSync(file, output);
-    });
-
-  // Now do an eslint fix parse
-  exec(`eslint --fix ${srcPath}`);
-};
-
-if (fs.existsSync('src/shared/universal/types')) {
-  rimraf('src/shared/universal/types', (err) => {
-    if (err) {
-      console.error('Could not remove "src/shared/universal/types" directory.');
-    } else {
-      stripTypes();
-    }
+// Strip the flow types from our src files.
+globSync(`${path.resolve(appRootPath, 'src')}/**/*.js`)
+  .filter(isJsFile)
+  .forEach((file) => {
+    console.log(`Removing types from "${file}`);
+    const input = fs.readFileSync(file, 'utf8');
+    const output =
+      // Remove flow annotations
+      flowRemoveTypes(input)
+      // Remove the empty flow tags.
+      .replace(/\/\*\s+\*\/\n/g, '')
+      // Remove any blank lines at top of file.
+      .replace(/^\n+/, '')
+      // Remove any multiple blank lines in files.
+      .replace(/\n\n\n/g, '\n\n');
+    fs.writeFileSync(file, output);
   });
-} else {
-  stripTypes();
-}
+
+// Delete the types folder in the src
+safeDelete(
+  flowTypesPath,
+  // Then do an eslint fix parse on the src files.
+  () => exec(`eslint --fix ${srcPath}`)
+);
+
+// Remove the .flowconfig
+safeDelete(flowConfigPath);
+
+// Remove the flow tools
+safeDelete(flowToolsPath);
+
+// Remove the flow script
+safeDelete(flowScriptPath);
+
+// Remove all flow items from the package.json
+fs.writeFileSync(
+  packageJsonPath,
+  fs.readFileSync(packageJsonPath, 'utf8')
+    .replace(/\s+"flow:?.*\n/g, '\n')
+    // Remove any multiple blank lines in files.
+    .replace(/\n\n+/g, '\n')
+    // Fix any hanging ',' chars
+    .replace(/,+([\s\n]+)\}/g, '$1}')
+);
