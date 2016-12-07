8ec156288f05eb0fef1e31906341dc01a393be5b
Sean Matheson <sean@ctrlplusb.com>
2016-12-07 19:44:20 +0000

    Changes the client asset resolver on the server to read the assets file on every request when in development mode.  This can avoid cases where a server doesn't register the new client assets when only the client bundle is built and restarted. Updates the babel-loader.

--- src/server/middleware/reactApplication/getAssetsForClientChunks.js
+++ src/server/middleware/reactApplication/getAssetsForClientChunks.js
@@ -19,7 +19,16 @@ if (!fs.existsSync(assetsFilePath)) {
   );
 }
 
-const assetsJSON = JSON.parse(fs.readFileSync(assetsFilePath, 'utf8'));
+const readAssetsJSONFile = () => JSON.parse(fs.readFileSync(assetsFilePath, 'utf8'));
+const assetsJSON = readAssetsJSONFile();
+const assetsJSONResolver = () => (
+  process.env.NODE_ENV === 'development'
+    // In development mode we always read the assets json file from disk to avoid
+    // any cases where an older version gets cached.
+    ? readAssetsJSONFile()
+    // Otherwise we return the initially parsed JSON file.
+    : assetsJSON
+);
 
 /**
  * Retrieves the js/css for the named chunks that belong to our client bundle.
@@ -36,7 +45,7 @@ const assetsJSON = JSON.parse(fs.readFileSync(assetsFilePath, 'utf8'));
  */
 function getAssetsForClientChunks(chunks: Array<string>) {
   return chunks.reduce((acc, chunkName) => {
-    const chunkAssets = assetsJSON[chunkName];
+    const chunkAssets = assetsJSONResolver()[chunkName];
     if (chunkAssets) {
       if (chunkAssets.js) {
         acc.js.push(chunkAssets.js);
--- src/shared/components/App/Home/Home.js
+++ src/shared/components/App/Home/Home.js
@@ -10,9 +10,9 @@ function Home() {
 
       <p>
         This starter kit contains all the build tooling and configuration you
-        need to kick off your next universal react project, whilst containing a
+        need to kick off your next universal React project, whilst containing a
         minimal project set up allowing you to make your own architecture
-        decisions (redux/mobx etc).
+        decisions (Redux/Mobx etc).
       </p>
     </article>
   );
