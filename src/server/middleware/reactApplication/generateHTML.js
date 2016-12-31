7c4c6fd4d7e62963404f1af76852ef258114d003
Sean Matheson <sean@ctrlplusb.com>
2016-12-31 07:17:26 +0000

    Prevents the Vendor DLL from being cached by the browser.

--- src/server/middleware/reactApplication/generateHTML.js
+++ src/server/middleware/reactApplication/generateHTML.js
@@ -107,7 +107,7 @@ export default function generateHTML(args) {
           // @see /tools/development/ensureVendorDLLExists.js
           process.env.NODE_ENV === 'development'
             && config.bundles.client.devVendorDLL.enabled
-            ? scriptTag(`${config.bundles.client.webPath}${config.bundles.client.devVendorDLL.name}.js`)
+            ? scriptTag(`${config.bundles.client.webPath}${config.bundles.client.devVendorDLL.name}.js?t=${Date.now()}`)
             : ''
         }
         ${scriptTags(assetsForRender.js)}
