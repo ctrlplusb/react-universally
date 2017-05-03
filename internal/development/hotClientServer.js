550e809ee17694d6f3234b7fe239342f593d7652
Dion Dirza <dion.dirza@gmail.com>
2017-05-03 17:14:19 +0700

    Fix CORS host (#432)

--- internal/development/hotClientServer.js
+++ internal/development/hotClientServer.js
@@ -2,6 +2,7 @@ import express from 'express';
 import createWebpackMiddleware from 'webpack-dev-middleware';
 import createWebpackHotMiddleware from 'webpack-hot-middleware';
 import ListenerManager from './listenerManager';
+import config from '../../config';
 import { log } from '../utils';
 
 class HotClientServer {
@@ -23,7 +24,7 @@ class HotClientServer {
       quiet: true,
       noInfo: true,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': `http://${config('host')}:${config('port')}`,
       },
       // Ensure that the public path is taken from the compiler webpack config
       // as it will have been created as an absolute path to avoid conflicts
